92e27b2c3a54777c538441ce218d4ec97591b8fa
Kenny Wang <kenny.wang@lombardrisk.com>
2016-01-13 16:36:27 +0800

    change baseUrl to server.url

--- src/main/java/org/yiwan/webcore/util/PropHelper.java
+++ src/main/java/org/yiwan/webcore/util/PropHelper.java
@@ -27,57 +27,55 @@ public class PropHelper {
 	}
 
 	// from base conf
-	public static final int TIMEOUT_INTERVAL = Integer.parseInt(props.getProperty("timeout.interval"));
-	public static final int TIMEOUT_NAVIGATION_INTERVAL = Integer
-			.parseInt(props.getProperty("timeout.navigation.interval"));
-	public static final int TIMEOUT_POLLING_INTERVAL = Integer.parseInt(props.getProperty("timeout.polling.interval"));
-	public static final int TIMEOUT_DOCUMENT_COMPLETE = Integer
-			.parseInt(props.getProperty("timeout.document.complete"));
-	public static final Integer TEST_RETRY_COUNT = Integer.valueOf(props.getProperty("test.retry.count"));
-	public static final String RANDOM_RULE = props.getProperty("random.rule");
-	public static final String RANDOM_SYMBOL = props.getProperty("random.symbol");
-	public static final String SOURCE_CODE_PATH = props.getProperty("sourcecode.path");
-	public static final String SOURCE_CODE_ENCODING = props.getProperty("sourcecode.encoding");
+	public static final int TIMEOUT_INTERVAL = Integer.parseInt(getProperty("timeout.interval"));
+	public static final int TIMEOUT_NAVIGATION_INTERVAL = Integer.parseInt(getProperty("timeout.navigation.interval"));
+	public static final int TIMEOUT_POLLING_INTERVAL = Integer.parseInt(getProperty("timeout.polling.interval"));
+	public static final int TIMEOUT_DOCUMENT_COMPLETE = Integer.parseInt(getProperty("timeout.document.complete"));
+	public static final Integer TEST_RETRY_COUNT = Integer.valueOf(getProperty("test.retry.count"));
+	public static final String RANDOM_RULE = getProperty("random.rule");
+	public static final String RANDOM_SYMBOL = getProperty("random.symbol");
+	public static final String SOURCE_CODE_PATH = getProperty("sourcecode.path");
+	public static final String SOURCE_CODE_ENCODING = getProperty("sourcecode.encoding");
 
 	// from biz conf
-	public static final int TIMEOUT_DIALOG_APPEAR = Integer.parseInt(props.getProperty("timeout.dialog.appear"));
-	public static final int TIMEOUT_DIALOG_DISAPPEAR = Integer.parseInt(props.getProperty("timeout.dialog.disappear"));
+	public static final int TIMEOUT_DIALOG_APPEAR = Integer.parseInt(getProperty("timeout.dialog.appear"));
+	public static final int TIMEOUT_DIALOG_DISAPPEAR = Integer.parseInt(getProperty("timeout.dialog.disappear"));
 
-	public static final String DEFAULT_LANG = props.getProperty("lang.default");
+	public static final String DEFAULT_LANG = getProperty("lang.default");
 
-	public static final String DATA_SCHEMA = props.getProperty("path.dataschema");
-	public static final String SYSTEM_DATA = props.getProperty("path.systemdata");
-	public static final String TEST_DATA_FOLDER = props.getProperty("path.testdata.folder");
-	public static final String MAPS_FOLDER = props.getProperty("path.maps.folder");
+	public static final String DATA_SCHEMA = getProperty("path.dataschema");
+	public static final String TEST_DATA_FOLDER = getProperty("path.testdata.folder");
+	public static final String MAPS_FOLDER = getProperty("path.maps.folder");
 
-	public static final String REMOTE_ADDRESS = props.getProperty("remote.address");
+	public static final String REMOTE_ADDRESS = getProperty("remote.address");
 
-	public static final String BROWSERSTACK_URL = props.getProperty("browserstackurl");
-	public static final String PROJECT = props.getProperty("project");
-	public static final String BUILD = props.getProperty("build");
-	public static final String BROWSERSTACK_LOCAL = props.getProperty("browserstacklocal");
-	public static final String BROWSERSTACK_LOCAL_IDENTIFIER = props.getProperty("browserstacklocalIdentifier");
-	public static final String BROWSERSTACK_DEBUG = props.getProperty("browserstackdebug");
+	public static final String BROWSERSTACK_URL = getProperty("browserstackurl");
+	public static final String PROJECT = getProperty("project");
+	public static final String BUILD = getProperty("build");
+	public static final String BROWSERSTACK_LOCAL = getProperty("browserstacklocal");
+	public static final String BROWSERSTACK_LOCAL_IDENTIFIER = getProperty("browserstacklocalIdentifier");
+	public static final String BROWSERSTACK_DEBUG = getProperty("browserstackdebug");
 
 	// from test conf
-	public static final String BASE_URL = props.getProperty("baseUrl");
+	public static final String BASE_URL = getProperty("server.url");
 
-	public static final String CURRENT_LANG = props.getProperty("lang.current");
+	public static final String CURRENT_LANG = getProperty("lang.current");
 
-	public static final String FIREFOX_PATH = props.getProperty("path.firefox");
-	public static final String CHROME_WEBDRIVER = props.getProperty("path.webdriver.chrome");
-	public static final String IE_WEBDRIVER_X86 = props.getProperty("path.webdriver.ie.x86");
-	public static final String IE_WEBDRIVER_X64 = props.getProperty("path.webdriver.ie.x64");
+	public static final String FIREFOX_PATH = getProperty("path.firefox");
+	public static final String CHROME_WEBDRIVER = getProperty("path.webdriver.chrome");
+	public static final String IE_WEBDRIVER_X86 = getProperty("path.webdriver.ie.x86");
+	public static final String IE_WEBDRIVER_X64 = getProperty("path.webdriver.ie.x64");
+	public static final String SYSTEM_DATA = getProperty("path.systemdata");
 
-	public static final String DEFAULT_OS = props.getProperty("os.default");
-	public static final String DEFAULT_OS_VERSION = props.getProperty("os.version.default");
-	public static final String DEFAULT_BROSWER = props.getProperty("broswer.default");
-	public static final String DEFAULT_IE_ARCH = props.getProperty("broswer.ie.arch.default");
-	public static final Boolean MAXIMIZE_BROSWER = Boolean.parseBoolean(props.getProperty("browser.maximize"));
+	public static final String DEFAULT_OS = getProperty("os.default");
+	public static final String DEFAULT_OS_VERSION = getProperty("os.version.default");
+	public static final String DEFAULT_BROSWER = getProperty("broswer.default");
+	public static final String DEFAULT_IE_ARCH = getProperty("broswer.ie.arch.default");
+	public static final Boolean MAXIMIZE_BROSWER = Boolean.parseBoolean(getProperty("browser.maximize"));
 
-	public static final Boolean REMOTE = Boolean.parseBoolean(props.getProperty("remote"));
-	public static final Boolean BROWSERSTACK = Boolean.parseBoolean(props.getProperty("browserstack"));
-	public static final Boolean ENABLE_HAR = Boolean.parseBoolean(props.getProperty("HAR.enable"));
+	public static final Boolean REMOTE = Boolean.parseBoolean(getProperty("remote"));
+	public static final Boolean BROWSERSTACK = Boolean.parseBoolean(getProperty("browserstack"));
+	public static final Boolean ENABLE_HAR = Boolean.parseBoolean(getProperty("HAR.enable"));
 
 	/**
 	 * load properties from external file
@@ -91,5 +89,15 @@ public class PropHelper {
 		} catch (IOException e) {
 			logger.warn("conf file " + path + " doesn't exist");
 		}
+	}
+
+	/**
+	 * get property from system first, if null then get from properties file
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public static String getProperty(String key) {
+		return System.getProperty(key) == null ? props.getProperty(key) : System.getProperty(key);
 	}
 }
