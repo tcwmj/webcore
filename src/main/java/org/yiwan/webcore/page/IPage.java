f9e879241dc200fe6c59651e413c19bb7c4fc246
Kenny Wang <kenny.wang@lombardrisk.com>
2016-01-20 17:57:14 +0800

    add locator unmarshal object

--- src/main/java/org/yiwan/webcore/page/IPage.java
+++ src/main/java/org/yiwan/webcore/page/IPage.java
@@ -1,5 +1,11 @@
 package org.yiwan.webcore.page;
 
-public interface IPage {
+import org.yiwan.webcore.locator.LocatorBean;
+import org.yiwan.webcore.util.JaxbHelper;
+import org.yiwan.webcore.util.PropHelper;
 
+public interface IPage {
+	public final static LocatorBean l = JaxbHelper.unmarshal(
+			ClassLoader.getSystemResourceAsStream(PropHelper.LOCATORS_FILE),
+			ClassLoader.getSystemResourceAsStream(PropHelper.LOCATOR_SCHEMA), LocatorBean.class);
 }
