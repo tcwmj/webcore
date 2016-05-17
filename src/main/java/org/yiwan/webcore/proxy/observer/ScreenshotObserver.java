44f25570491deb41f25e0e092831fbcdd77caf55
Kenny Wang <kenny.wang@lombardrisk.com>
2016-05-17 10:43:34 +0800

    finish ScreenshotObserver

--- src/main/java/org/yiwan/webcore/proxy/observer/ScreenshotObserver.java
+++ src/main/java/org/yiwan/webcore/proxy/observer/ScreenshotObserver.java
@@ -1,11 +1,16 @@
 package org.yiwan.webcore.proxy.observer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.yiwan.webcore.test.ITestBase;
+
+import java.io.IOException;
 
 /**
  * Created by Kenny Wang on 4/5/2016.
  */
 public class ScreenshotObserver extends SampleObserver {
+    private static final Logger logger = LoggerFactory.getLogger(ScreenshotObserver.class);
     private ITestBase testCase;
 
     public ScreenshotObserver(ITestBase testCase) {
@@ -20,5 +25,10 @@ public class ScreenshotObserver extends SampleObserver {
     @Override
     public void stop() {
         super.stop();
+        try {
+            testCase.embedScreenshot();
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
     }
 }
