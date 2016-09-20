6ff9f7c87783a8736f10e4ed7f282f6a9020db04
Kenny Wang <kenny.wang@lombardrisk.com>
2016-09-20 14:30:57 +0800

    add some comments in base.properties

--- src/main/java/org/yiwan/webcore/util/PropHelper.java
+++ src/main/java/org/yiwan/webcore/util/PropHelper.java
@@ -52,7 +52,7 @@ public class PropHelper {
     public static final boolean HTTP_STATUS_TO_204 = Boolean.parseBoolean(getProperty("http.status.to.204"));
     public static final boolean ENABLE_HTTP_ARCHIVE = Boolean.parseBoolean(getProperty("har.enable"));
     public static final boolean ENABLE_TRANSACTION_TIMESTAMP_RECORD = Boolean.parseBoolean(getProperty("record.transaction.timestamp.enable"));
-    public static final boolean ENABLE_TRANSACTION_SCREENSHOT_CAPTURE = Boolean.parseBoolean(getProperty("capture.transaction.screeshot.enable"));
+    public static final boolean ENABLE_TRANSACTION_SCREENSHOT_CAPTURE = Boolean.parseBoolean(getProperty("capture.transaction.screenshot.enable"));
     public static final boolean ENABLE_PENETRATION_TEST = Boolean.parseBoolean(getProperty("penetration.test.enable"));
     public static final boolean ENABLE_XSD_VALIDATION = Boolean.parseBoolean(getProperty("xsd.validation.enable"));
 
