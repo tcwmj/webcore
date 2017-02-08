c68969f4af48be685c56d06fea7b57031dfb8889
Kenny Wang <kenny.wang@lombardrisk.com>
2017-02-08 14:39:04 +0800

    add new property action.max.retry.times

--- src/main/java/org/yiwan/webcore/web/WebDriverActionExecutor.java
+++ src/main/java/org/yiwan/webcore/web/WebDriverActionExecutor.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yiwan.webcore.util.ActionExecutor;
 import org.yiwan.webcore.util.IAction;
+import org.yiwan.webcore.util.PropHelper;
 
 /**
  * Created by Kenny Wang on 8/22/2016.
@@ -14,7 +15,7 @@ public class WebDriverActionExecutor extends ActionExecutor {
     private final Logger logger = LoggerFactory.getLogger(WebDriverActionExecutor.class);
 
     public WebDriverActionExecutor() {
-        super(2);
+        super(Integer.parseInt(PropHelper.getProperty("action.max.retry.times")));
     }
 
     public WebDriverActionExecutor(int max_retry_times) {
